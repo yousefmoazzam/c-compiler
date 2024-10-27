8487a27882de75cd62adeaaa9607fbaa1a08f28b
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2024-10-27 18:45:21 +0000

    Panic if no match to the current substring is found

--- src/lex.rs
+++ src/lex.rs
@@ -50,6 +50,16 @@ pub fn lex(text: &str) -> Vec<Token> {
                 }
                 continue;
             }
+
+            // No match was found, so the string contains either:
+            // - valid C code, but not yet supported
+            // - invalid C code
+            //
+            // These cases should be handled differently, but for now, panic for both
+            panic!(
+                "No match found for the following substring: {}",
+                &line[idx..]
+            )
         }
     }
 
@@ -74,5 +84,12 @@ mod tests {
         let expected_tokens = vec![Token::IntKeyword, Token::Identifier("main".to_string())];
         let tokens = lex(source_code_string);
         assert_eq!(tokens, expected_tokens);
+    }
+
+    #[test]
+    #[should_panic(expected = "No match found for the following substring: ?")]
+    fn panic_if_no_match_found_for_substring() {
+        let source_code_string = "?";
+        lex(source_code_string);
     }
 }
