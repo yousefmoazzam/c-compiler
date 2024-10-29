efa54a4056aa6ae765916ff2949f68c2879efd5b
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2024-10-29 23:00:26 +0000

    Parse associated tokens into function definition AST node

--- src/parse.rs
+++ src/parse.rs
@@ -2,6 +2,8 @@ use std::collections::VecDeque;
 
 use crate::lex::Token;
 
+type Identifier = String;
+
 #[derive(Debug, PartialEq)]
 pub enum Expression {
     NumericConstant(u8),
@@ -10,6 +12,11 @@ pub enum Expression {
 #[derive(Debug, PartialEq)]
 pub enum Statement {
     Return(Expression),
+}
+
+#[derive(Debug, PartialEq)]
+pub enum FunctionDefinition {
+    Function { name: Identifier, body: Statement },
 }
 
 pub fn parse_expression(tokens: &mut VecDeque<Token>) -> Expression {
@@ -49,6 +56,58 @@ pub fn parse_statement(tokens: &mut VecDeque<Token>) -> Statement {
     Statement::Return(expression_ast_node)
 }
 
+pub fn parse_function_definition(tokens: &mut VecDeque<Token>) -> FunctionDefinition {
+    let next_token = tokens
+        .pop_front()
+        .expect("Should have non-empty queue of tokens");
+    if next_token != Token::IntKeyword {
+        todo!()
+    }
+
+    let next_token = tokens
+        .pop_front()
+        .expect("Should have non-empty queue of tokens");
+    let identifier = match next_token {
+        Token::Identifier(identifier) => identifier,
+        _ => todo!(),
+    };
+
+    let next_token = tokens
+        .pop_front()
+        .expect("Should have non-empty queue of tokens");
+    if next_token != Token::OpenParenthesis {
+        todo!()
+    }
+
+    let next_token = tokens
+        .pop_front()
+        .expect("Should have non-emtyp queue of tokens");
+    if next_token != Token::CloseParenthesis {
+        todo!()
+    }
+
+    let next_token = tokens
+        .pop_front()
+        .expect("Should have non-empty queue of tokens");
+    if next_token != Token::OpenBrace {
+        todo!()
+    }
+
+    let statement_ast_node = parse_statement(tokens);
+
+    let next_token = tokens
+        .pop_front()
+        .expect("Should have non-empty queue of tokens");
+    if next_token != Token::CloseBrace {
+        todo!()
+    }
+
+    FunctionDefinition::Function {
+        name: identifier.to_string(),
+        body: statement_ast_node,
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -73,6 +132,32 @@ mod tests {
         ]);
         let expected_ast_node = Statement::Return(Expression::NumericConstant(value));
         let ast_node = parse_statement(&mut tokens);
+        assert_eq!(0, tokens.len());
+        assert_eq!(ast_node, expected_ast_node);
+    }
+
+    #[test]
+    fn parse_function_defn_with_int_return_and_statement_as_body() {
+        let value = 2;
+        let identifier = "main";
+        let mut tokens = VecDeque::from([
+            Token::IntKeyword,
+            Token::Identifier(identifier.to_string()),
+            Token::OpenParenthesis,
+            Token::CloseParenthesis,
+            Token::OpenBrace,
+            Token::ReturnKeyword,
+            Token::NumericConstant(value),
+            Token::Semicolon,
+            Token::CloseBrace,
+        ]);
+        let expression_ast_node = Expression::NumericConstant(value);
+        let statement_ast_node = Statement::Return(expression_ast_node);
+        let expected_ast_node = FunctionDefinition::Function {
+            name: identifier.to_string(),
+            body: statement_ast_node,
+        };
+        let ast_node = parse_function_definition(&mut tokens);
         assert_eq!(0, tokens.len());
         assert_eq!(ast_node, expected_ast_node);
     }
