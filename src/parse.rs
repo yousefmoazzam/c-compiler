78e78d94e33238cb990afd6d758b7a4a8de5c420
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2024-10-29 22:46:32 +0000

    Change parsing functions to receive mutable queue of tokens

--- src/parse.rs
+++ src/parse.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 use crate::lex::Token;
 
 #[derive(Debug, PartialEq)]
@@ -10,26 +12,37 @@ pub enum Statement {
     Return(Expression),
 }
 
-pub fn parse_expression(tokens: &[Token]) -> Expression {
-    let next_token = &tokens[0];
+pub fn parse_expression(tokens: &mut VecDeque<Token>) -> Expression {
+    // The queue of tokens shouldn't be empty if the queue has been handled correctly by others, so
+    // the panic shouldn't occur. Hence, the use of `expect()`.
+    let next_token = tokens
+        .pop_front()
+        .expect("Should have non-empty queue of tokens");
     if let Token::NumericConstant(val) = next_token {
-        return Expression::NumericConstant(*val);
+        return Expression::NumericConstant(val);
     }
 
     // TODO: Handle if the token isn't an expected variant
     todo!()
 }
 
-pub fn parse_statement(tokens: &[Token]) -> Statement {
-    let first_token = &tokens[0];
-    if *first_token != Token::ReturnKeyword {
+pub fn parse_statement(tokens: &mut VecDeque<Token>) -> Statement {
+    // The queue of tokens shouldn't be empty if the queue has been handled correctly by others, so
+    // the panic shouldn't occur. Hence, the use of `expect()`.
+    let first_token = tokens
+        .pop_front()
+        .expect("Should have non-empty queue of tokens");
+    println!("first_token is {:?}", first_token);
+    if first_token != Token::ReturnKeyword {
         todo!()
     }
 
-    let expression_ast_node = parse_expression(&tokens[1..]);
+    let expression_ast_node = parse_expression(tokens);
 
-    let third_token = &tokens[2];
-    if *third_token != Token::Semicolon {
+    let third_token = tokens
+        .pop_front()
+        .expect("Should have non-empty queue of tokens");
+    if third_token != Token::Semicolon {
         todo!()
     }
 
@@ -43,22 +56,24 @@ mod tests {
     #[test]
     fn parse_expression_containing_numeric_constant() {
         let value = 2;
-        let tokens = vec![Token::NumericConstant(value)];
+        let mut tokens = VecDeque::from([Token::NumericConstant(value)]);
         let expected_ast_node = Expression::NumericConstant(value);
-        let ast_node = parse_expression(&tokens[..]);
+        let ast_node = parse_expression(&mut tokens);
+        assert_eq!(0, tokens.len());
         assert_eq!(ast_node, expected_ast_node);
     }
 
     #[test]
     fn parse_statement_with_return_identifier_and_numeric_expression() {
         let value = 2;
-        let tokens = vec![
+        let mut tokens = VecDeque::from([
             Token::ReturnKeyword,
             Token::NumericConstant(value),
             Token::Semicolon,
-        ];
+        ]);
         let expected_ast_node = Statement::Return(Expression::NumericConstant(value));
-        let ast_node = parse_statement(&tokens[..]);
+        let ast_node = parse_statement(&mut tokens);
+        assert_eq!(0, tokens.len());
         assert_eq!(ast_node, expected_ast_node);
     }
 }
