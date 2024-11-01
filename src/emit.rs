766720264a29353102dff3facffba5e23d545f68
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2024-11-01 17:00:23 +0000

    Convert assembly function definition function variant to string

--- src/emit.rs
+++ src/emit.rs
@@ -1,4 +1,4 @@
-use crate::parse::asm::{Instruction, Operand};
+use crate::parse::asm::{FunctionDefinition, Instruction, Operand};
 
 pub fn emit_operand(node: Operand) -> String {
     match node {
@@ -15,6 +15,19 @@ pub fn emit_instruction(node: Instruction) -> String {
             format!("    movl {}, {}", src_string, dst_string)
         }
         Instruction::Ret => "    ret".to_string(),
+    }
+}
+
+pub fn emit_function_definition(node: FunctionDefinition) -> Vec<String> {
+    match node {
+        FunctionDefinition::Function { name, instructions } => {
+            let mut lines = vec![format!("    .globl {}", name), format!("{}", name)];
+            for instruction in instructions {
+                let instruction_string = emit_instruction(instruction);
+                lines.push(instruction_string);
+            }
+            lines
+        }
     }
 }
 
@@ -56,6 +69,31 @@ mod tests {
         let ast_node = Instruction::Ret;
         let asm_code = emit_instruction(ast_node);
         let expected_asm_code = "    ret";
+        assert_eq!(asm_code, expected_asm_code);
+    }
+
+    #[test]
+    fn emit_function_definition_returns_correct_vector_of_strings() {
+        let value = 2;
+        let identifier = "main";
+        let instructions = vec![
+            Instruction::Mov {
+                src: Operand::Imm(value),
+                dst: Operand::Register,
+            },
+            Instruction::Ret,
+        ];
+        let ast_node = FunctionDefinition::Function {
+            name: identifier.to_string(),
+            instructions,
+        };
+        let asm_code = emit_function_definition(ast_node);
+        let expected_asm_code = vec![
+            format!("    .globl {}", identifier.to_string()),
+            format!("{}", identifier.to_string()),
+            format!("    movl ${}, %eax", value),
+            "    ret".to_string(),
+        ];
         assert_eq!(asm_code, expected_asm_code);
     }
 }
