8be5121b99294377d20556fbee062b06386e74c2
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2024-11-01 01:50:12 +0000

    Convert assembly instruction mov variant to string

--- src/emit.rs
+++ src/emit.rs
@@ -1,9 +1,20 @@
-use crate::parse::asm::Operand;
+use crate::parse::asm::{Instruction, Operand};
 
 pub fn emit_operand(node: Operand) -> String {
     match node {
         Operand::Imm(val) => format!("${}", val),
         Operand::Register => "%eax".to_string(),
+    }
+}
+
+pub fn emit_instruction(node: Instruction) -> String {
+    match node {
+        Instruction::Mov { src, dst } => {
+            let src_string = emit_operand(src);
+            let dst_string = emit_operand(dst);
+            format!("    movl {}, {}", src_string, dst_string)
+        }
+        _ => todo!(),
     }
 }
 
@@ -25,6 +36,18 @@ mod tests {
         let ast_node = Operand::Register;
         let asm_code = emit_operand(ast_node);
         let expected_asm_code = "%eax";
+        assert_eq!(asm_code, expected_asm_code);
+    }
+
+    #[test]
+    fn emit_mov_instruction() {
+        let value = 2;
+        let ast_node = Instruction::Mov {
+            src: Operand::Imm(value),
+            dst: Operand::Register,
+        };
+        let asm_code = emit_instruction(ast_node);
+        let expected_asm_code = "    movl $2, %eax";
         assert_eq!(asm_code, expected_asm_code);
     }
 }
