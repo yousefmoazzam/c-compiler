58d7d57a48829d95bdefe265ad086a2f0e849956
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2024-11-01 01:39:34 +0000

    Convert assembly register operand variant to string

--- src/emit.rs
+++ src/emit.rs
@@ -3,7 +3,7 @@ use crate::parse::asm::Operand;
 pub fn emit_operand(node: Operand) -> String {
     match node {
         Operand::Imm(val) => format!("${}", val),
-        _ => todo!(),
+        Operand::Register => "%eax".to_string(),
     }
 }
 
@@ -17,6 +17,14 @@ mod tests {
         let ast_node = Operand::Imm(value);
         let asm_code = emit_operand(ast_node);
         let expected_asm_code = "$2";
+        assert_eq!(asm_code, expected_asm_code);
+    }
+
+    #[test]
+    fn emit_register_operand() {
+        let ast_node = Operand::Register;
+        let asm_code = emit_operand(ast_node);
+        let expected_asm_code = "%eax";
         assert_eq!(asm_code, expected_asm_code);
     }
 }
