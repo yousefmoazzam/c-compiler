e8463ccb8ed5b896cec6e8fce096de312a0edefd
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2024-11-06 21:44:35 +0000

    Parse C complement operator into IR unary operator AST node

--- src/parse/ir.rs
+++ src/parse/ir.rs
@@ -1,8 +1,20 @@
 use crate::parse::c;
 
 #[derive(Debug, PartialEq)]
+pub enum UnaryOperator {
+    BitwiseComplement,
+}
+
+#[derive(Debug, PartialEq)]
 pub enum Value {
     Constant(u8),
+}
+
+pub fn parse_unary_operator(node: c::UnaryOperator) -> UnaryOperator {
+    match node {
+        c::UnaryOperator::BitwiseComplement => UnaryOperator::BitwiseComplement,
+        _ => todo!(),
+    }
 }
 
 pub fn parse_value(node: c::Expression) -> Value {
@@ -22,6 +34,14 @@ mod tests {
         let c_ast_node = c::Expression::NumericConstant(value);
         let expected_ir_ast_node = Value::Constant(value);
         let ir_ast_node = parse_value(c_ast_node);
+        assert_eq!(ir_ast_node, expected_ir_ast_node);
+    }
+
+    #[test]
+    fn parse_c_complement_operstor_to_ir_unary_operator() {
+        let c_ast_node = c::UnaryOperator::BitwiseComplement;
+        let expected_ir_ast_node = UnaryOperator::BitwiseComplement;
+        let ir_ast_node = parse_unary_operator(c_ast_node);
         assert_eq!(ir_ast_node, expected_ir_ast_node);
     }
 }
